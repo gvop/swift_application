626893097c9336ec08d59fd2439c31c84d3b9258
gvop <guusvanooijen@gmail.com>
2016-01-25 21:13:05 +0000

    Finding out the differtent data types, how to write a function and an 'else if'-statment

--- playground/MyPlayground.playground/Contents.swift
+++ playground/MyPlayground.playground/Contents.swift
@@ -4,4 +4,40 @@ import UIKit
 
 var str = "Hello, playground"
 
-var message = "Hello World"
+var message = "Hello Guus"
+
+
+//This is a comment
+print(message)
+
+print(message, terminator: "")
+
+var a = 10
+
+var b = 20
+
+print(a + b)
+
+var total = a + b
+
+//Function with String as data type
+func sayHello(personName: String) -> String {
+    let greeting = "Hello, " + personName + "!"
+    return greeting
+}
+
+print(sayHello("Anna"))
+
+
+//Function with number as data type
+if (total < 20) {
+    print("more then 20")
+} else {
+    print("les then 20")
+}
+
+func sum(total: Int) -> Int {
+    return total
+}
+
+print(sum(40))
