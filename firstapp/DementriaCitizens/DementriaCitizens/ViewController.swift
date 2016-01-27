16265e672a2197ee9962ee9a0cc8efaefbdb9111
gvop <guusvanooijen@gmail.com>
2016-01-27 12:28:59 +0000

    Setting up dementia project, makind different view. First focusing on the page changes

---
+++ firstapp/DementriaCitizens/DementriaCitizens/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DementriaCitizens
+//
+//  Created by Guus van Ooijen on 27/01/2016.
+//  Copyright © 2016 SP+EE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        joinButton.layer.cornerRadius = 5
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
