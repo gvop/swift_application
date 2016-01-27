35edc09f8e28a004e8ae4cd67f5c6985cc8b314d
gvop <guusvanooijen@gmail.com>
2016-01-27 13:31:58 +0000

    After some research created controllr for a new view

---
+++ firstapp/DementriaCirizens/DementriaCirizens/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  DementriaCirizens
+//
+//  Created by Guus van Ooijen on 27/01/2016.
+//  Copyright © 2016 gvop. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var secondButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ firstapp/DementriaCirizens/DementriaCirizens/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DementriaCirizens
+//
+//  Created by Guus van Ooijen on 27/01/2016.
+//  Copyright © 2016 gvop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
